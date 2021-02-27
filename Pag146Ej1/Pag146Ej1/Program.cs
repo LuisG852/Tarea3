ddb4b6aba0240c393226c9ef753c994d7f879c54
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-02-26 23:26:14 -0600

    Add files via upload

---
+++ Pag146Ej1/Pag146Ej1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pag146Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dato;
+
+            Console.WriteLine("Ingresa el numero de la tabla que desees consultar:");
+            dato = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(dato + "x" + i + "=" + (i * dato));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
