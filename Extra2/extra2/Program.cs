cf7163279400513cec682d0b4671ff9d747428fd
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-02-27 00:02:17 -0600

    Add files via upload

---
+++ Extra2/extra2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace extra2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, numero;
+             long factorial = 1;
+             Console.Write("Ingrese un número:");
+             numero = int.Parse(Console.ReadLine());
+             Console.WriteLine("\n" + numero + "! = 1");
+             for (i = 2; i <= numero; i++)
+            {
+                factorial *= i;
+                Console.WriteLine("\u00D7" + i);
+            }
+            Console.WriteLine("=" + factorial);
+            Console.WriteLine("Presione cualquier tecla para terminar...");
+            Console.ReadKey();
+        }
+    }
+}
