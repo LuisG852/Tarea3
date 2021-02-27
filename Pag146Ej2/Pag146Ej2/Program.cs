42829ae2ca65aa387c2268fc958b60ee3f2d699c
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-02-26 23:29:38 -0600

    Add files via upload

---
+++ Pag146Ej2/Pag146Ej2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pag146Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Base, Exponente;
+
+            Console.WriteLine("Ingresa la base de la operacion: ");
+            Base = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Ahora ingrese el exponente a elevarlo.");
+            Exponente = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine($"Base: {Base}, Exponente {Exponente}. -> {Base}^{Exponente}\nResultado: {Math.Pow(Base, Exponente)}");
+
+            Console.ReadKey();
+        }
+    }
+}
